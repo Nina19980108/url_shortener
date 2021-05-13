c3015d0b0c08144e7bab6681fc7fc52a159f43ed
Nina19980108 <ninagirl8787@gmail.com>
2021-05-13 09:05:25 +0800

    feat: add randomURL.hbs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const randomURL = require('./randomURL')
+const RandomURL = require('./randomURL')
 
 const app = express()
 const port = 3000
+let url = ''
+let randomURL = ''
+const pages = []
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
@@ -13,10 +16,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/url', (req, res) => {
-  const url = req.query.url
-  console.log(url)
-  console.log(randomURL())
-  res.render('url', { url })
+  url = req.query.url
+  console.log(pages.some(page => page[0] === url))
+
+  randomURL = RandomURL()
+  pages.push([url, randomURL])
+  console.log(pages)
+  res.render('url', { randomURL })
+})
+
+app.get('/url/:randomURL', (req, res) => {
+  const randomURL = req.query.randomURL
+  res.render('randomURL', { randomURL })
 })
 
 app.listen(port, () => {
