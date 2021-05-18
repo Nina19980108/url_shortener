8671e4ea3a2d6604384b370af7ba84ac14408c4c
Nina19980108 <ninagirl8787@gmail.com>
2021-05-18 10:17:46 +0800

    chore:heroku init

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const routes = require('./routes/index')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(routes)
 
 
-app.listen(port, () => {
-  console.log(`App is listening on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`App is listening on http://localhost:${PORT}`)
 })
